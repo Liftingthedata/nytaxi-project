1f608d555eb7d274e8c33681e31ebc5501d80062
Islam Elsayed <107177143+Liftingthedata@users.noreply.github.com>
2022-11-14 20:27:34 +0200

    Update full-refresh.py

--- airflow/dags/full-refresh.py
+++ airflow/dags/full-refresh.py
@@ -13,28 +13,28 @@ from airflow.providers.google.cloud.operators.kubernetes_engine import (
     GKEStartPodOperator,
 )
 # from google.auth import compute_engine
-os.environ['AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT']='google-cloud-platform://'
+# os.environ['AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT']='google-cloud-platform://'
   
-from google.oauth2 import service_account
-from googleapiclient import discovery
+# from google.oauth2 import service_account
+# from googleapiclient import discovery
 
-#Downloaded credentials in JSON format
-gcp_sa_credentials={
-  "type": "service_account",
-  "project_id": "stella-luxury-taxi",
-  "private_key_id": "e6897c764237693277647217f46edcc971d6fa03",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCp7TdAB7pjalXw\nlrv69Jo+oOUljranFpqJNiGcQ3NJEhEbN7XFDBG8unn+vZwPwE0cfTk11C/B2WLR\nZwzJf4ivQ7IqlNKgYWbQJGpFCwDX1YnSNFbZyNddMyg0FuEvuQ//xSBTfrhb6Vl2\nQ9YQLovFHn40XTs+NsTLQZdeknOEdsA8aMR74/Q71EJP+RnMJ8uZnWIIpEVn1TyE\n/fLOBePTUMwgEtPkhsHWycyW4u9DQi7wcp2zNA+1nBb7I34laAf/cLFdLU3MSDuH\nz/nMHpTHzEbSaIpjYqFOQfgGEUn86g46/YoA8/J3/cOKG0WVXIFNJ5bVST1zgEHP\nIM2gOq77AgMBAAECggEACs6TXPcg24G/xXVQrz0vMhx5dlIFO8ssON3AdXe0tUj4\n3YpFfSqvVhll+NWGP1ozjURJhyrffqycpESxg38g6kSb6Cle2+RV7ZbjS1DP1Oo3\nwa6id2dWiw7d17I80BQs+E9JJwZAI1hL4EGgM5dCPF8cF6h2RBannWWmgtU9k4b/\nGz2dznZoQMVN/q09zYW/jeQbvkvldSfvuuJH3ucMPwGXVZdEPpz9i5ut9tc/af6J\nn0H8f67PMD8X3kkDCYj4IL1KCNEY7+ftoY7+Imd3FRH6uPEw17FkEYFqFb/FIxRi\nm8lYj/MjswBdHZCuyqMIFI+xgQXPkFf/dzXOAyLAwQKBgQDvQF8lPUOZ7GSx+Dni\n2Ik+7OGkJC8cR29Qbv4Wsz11bzqNTvNajdGTUQW2KF3L84D7qsLVvgbfAmczFMDx\nGeV8VdXhNkLjxsCLqYxU7S+gN8ifCZwVCMPSEa9kDdir52UZzLwH1sHYzd4LPdra\nKl+WMo0VeTXt15Sr3z71Q713tQKBgQC10nkY2iErMKFEbajUBJlS9jZ7aSXSFzjo\neSGYOVBYZi9oJD/KizUsB2kWxK4HOkKyq6sgyrdVHm3hmqk52SUVoWkD3IcNF6hE\nqOm+Vf2a2yColnB2xhd0KBcGgi4fIy8buqpkmn2Lxl+XAyx/XCKWH4fzzZ9QNrn4\nQhuBVRJZ7wKBgQDSbCNCdWeHcUn+3PrMcPYEygKKguiMTqewbm47ONnM907gCZgv\nBJxWnOQRGd+lCT1gGwfRRZh1e3+YhaBMbSJRAI1jzn12J9AhBbXO2+0PVQC2H5WP\nSm4vzC4eKa9vQczBrDeUDWXgcO/hoz1gs/Pt/ffn8vtjfD/eCjMtM67oIQKBgCSK\ndyHifLYEYPSyoTJy9ilxKAPnXt15I0u9RF4mbppFdxOT7WoUTgxaNOmJf3weXlcw\nHwVJGE03/1dO0OG6XTSaqtNG17Fu5rddxxQkjgI4NbkL+vAz4XTLtczuDrzdQlNt\nUV9EmSSlKoLb9W5nIuBO1/DMi08AoKFfD84PPc/rAoGBALBmwjsygh4HbO1hrbdT\n2Q44LZpYL0PRCMLVRGtaBh10/TinQuie+pMa+sqrpNb0T9NnE6n3NN9ewDbTZ279\njkuOQVpoHZPWGwGC0ki9aupDK7Sr5RcUT9vMyN5GtSky8DwkKrRPUX/3QIR9LSDA\n9rUdxCeXAYlsYkK4m8mDJUIz\n-----END PRIVATE KEY-----\n",
-  "client_email": "gke-sa@stella-luxury-taxi.iam.gserviceaccount.com",
-  "client_id": "106181116653793762876",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gke-sa%40stella-luxury-taxi.iam.gserviceaccount.com"
-}
+# #Downloaded credentials in JSON format
+# gcp_sa_credentials={
+#   "type": "service_account",
+#   "project_id": "stella-luxury-taxi",
+#   "private_key_id": "e6897c764237693277647217f46edcc971d6fa03",
+#   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCp7TdAB7pjalXw\nlrv69Jo+oOUljranFpqJNiGcQ3NJEhEbN7XFDBG8unn+vZwPwE0cfTk11C/B2WLR\nZwzJf4ivQ7IqlNKgYWbQJGpFCwDX1YnSNFbZyNddMyg0FuEvuQ//xSBTfrhb6Vl2\nQ9YQLovFHn40XTs+NsTLQZdeknOEdsA8aMR74/Q71EJP+RnMJ8uZnWIIpEVn1TyE\n/fLOBePTUMwgEtPkhsHWycyW4u9DQi7wcp2zNA+1nBb7I34laAf/cLFdLU3MSDuH\nz/nMHpTHzEbSaIpjYqFOQfgGEUn86g46/YoA8/J3/cOKG0WVXIFNJ5bVST1zgEHP\nIM2gOq77AgMBAAECggEACs6TXPcg24G/xXVQrz0vMhx5dlIFO8ssON3AdXe0tUj4\n3YpFfSqvVhll+NWGP1ozjURJhyrffqycpESxg38g6kSb6Cle2+RV7ZbjS1DP1Oo3\nwa6id2dWiw7d17I80BQs+E9JJwZAI1hL4EGgM5dCPF8cF6h2RBannWWmgtU9k4b/\nGz2dznZoQMVN/q09zYW/jeQbvkvldSfvuuJH3ucMPwGXVZdEPpz9i5ut9tc/af6J\nn0H8f67PMD8X3kkDCYj4IL1KCNEY7+ftoY7+Imd3FRH6uPEw17FkEYFqFb/FIxRi\nm8lYj/MjswBdHZCuyqMIFI+xgQXPkFf/dzXOAyLAwQKBgQDvQF8lPUOZ7GSx+Dni\n2Ik+7OGkJC8cR29Qbv4Wsz11bzqNTvNajdGTUQW2KF3L84D7qsLVvgbfAmczFMDx\nGeV8VdXhNkLjxsCLqYxU7S+gN8ifCZwVCMPSEa9kDdir52UZzLwH1sHYzd4LPdra\nKl+WMo0VeTXt15Sr3z71Q713tQKBgQC10nkY2iErMKFEbajUBJlS9jZ7aSXSFzjo\neSGYOVBYZi9oJD/KizUsB2kWxK4HOkKyq6sgyrdVHm3hmqk52SUVoWkD3IcNF6hE\nqOm+Vf2a2yColnB2xhd0KBcGgi4fIy8buqpkmn2Lxl+XAyx/XCKWH4fzzZ9QNrn4\nQhuBVRJZ7wKBgQDSbCNCdWeHcUn+3PrMcPYEygKKguiMTqewbm47ONnM907gCZgv\nBJxWnOQRGd+lCT1gGwfRRZh1e3+YhaBMbSJRAI1jzn12J9AhBbXO2+0PVQC2H5WP\nSm4vzC4eKa9vQczBrDeUDWXgcO/hoz1gs/Pt/ffn8vtjfD/eCjMtM67oIQKBgCSK\ndyHifLYEYPSyoTJy9ilxKAPnXt15I0u9RF4mbppFdxOT7WoUTgxaNOmJf3weXlcw\nHwVJGE03/1dO0OG6XTSaqtNG17Fu5rddxxQkjgI4NbkL+vAz4XTLtczuDrzdQlNt\nUV9EmSSlKoLb9W5nIuBO1/DMi08AoKFfD84PPc/rAoGBALBmwjsygh4HbO1hrbdT\n2Q44LZpYL0PRCMLVRGtaBh10/TinQuie+pMa+sqrpNb0T9NnE6n3NN9ewDbTZ279\njkuOQVpoHZPWGwGC0ki9aupDK7Sr5RcUT9vMyN5GtSky8DwkKrRPUX/3QIR9LSDA\n9rUdxCeXAYlsYkK4m8mDJUIz\n-----END PRIVATE KEY-----\n",
+#   "client_email": "gke-sa@stella-luxury-taxi.iam.gserviceaccount.com",
+#   "client_id": "106181116653793762876",
+#   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#   "token_uri": "https://oauth2.googleapis.com/token",
+#   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gke-sa%40stella-luxury-taxi.iam.gserviceaccount.com"
+# }
 
-project_id=gcp_sa_credentials["project_id"]
+# project_id=gcp_sa_credentials["project_id"]
 
-credentials = service_account.Credentials.from_service_account_info(gcp_sa_credentials)
+# credentials = service_account.Credentials.from_service_account_info(gcp_sa_credentials)
 
     
 logger = logging.getLogger(__name__)
@@ -45,36 +45,7 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.bash_operator import BashOperator
-import time
 
-dag_name = 'dagName'
-
-default_args = {
-    'owner': 'Airflow',
-    'start_date':datetime(2022,2,16),
-    'schedule_interval':'@once',
-    'retries': 1,
-    'retry_delay': timedelta(seconds=5),
-    'depends_on_past': False,
-    'catchup':False
-}
-
-with DAG(dag_name,default_args=default_args) as dag:
-
-    t1 = DummyOperator(task_id="start")
-
-    t2 = BashOperator(task_id='hello_world',
-                        bash_command='echo "Hi!!"')
-    
-    t3 = DummyOperator(task_id="end")
-    
-    t1 >> t2 >> t3
-
-# # credentials = compute_engine.Credentials()
 # aws_secret = Secret(
 #     deploy_type='volume',
 #     # Path where we mount the secret as volume
@@ -93,41 +64,41 @@ with DAG(dag_name,default_args=default_args) as dag:
 
 # # os.environ['AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT'] = f'google-cloud-platform://?extra__google_cloud_platform__key_secret_name={gcp_secret}'
 
-# default_args = {
-#     "owner": "airflow",
-#     "start_date": datetime.now(),
-#     "depends_on_past": False,
-#     "retries": 2,
-#     "retry_delay": timedelta(minutes=60),
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime.now(),
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=60),
    
-# }
+}
 
-# with DAG(
-#     dag_id="full-refresh",
-#     schedule_interval=None,
-#     default_args=default_args,
-#     catchup=False,
-#     tags=["full-refresh"],
-# ) as dag:
-#     CLUSTER_NAME = "gke-cluster"
-#     PROJECT = os.getenv("PROJECT")
-#     CLUSTER_REGION = os.getenv("CLUSTER_REGION")
-#     STAGING_BUCKET = os.getenv("STAGING_BUCKET")
+with DAG(
+    dag_id="full-refresh",
+    schedule_interval=None,
+    default_args=default_args,
+    catchup=False,
+    tags=["full-refresh"],
+) as dag:
+    CLUSTER_NAME = "gke-cluster"
+    PROJECT = os.getenv("PROJECT")
+    CLUSTER_REGION = os.getenv("CLUSTER_REGION")
+    STAGING_BUCKET = os.getenv("STAGING_BUCKET")
 
-#     from_s3_to_gcs = GKEStartPodOperator(
-#         # The ID specified for the task.
-#         task_id="data-transfer-task",
-#         # Name of task you want to run, used to generate Pod ID.
-#         name="data-transfer-task",
-#         project_id=PROJECT,
-#         location=CLUSTER_REGION,  # type: ignore
-#         cluster_name=CLUSTER_NAME,
-#         cmds=["/bin/bash", "./extract_data.sh", "yellow"],
-#         namespace="default",
-#         image="eu.gcr.io/stella-luxury-taxi/transfer-pod1",
-#         secrets=[gcp_secret],
-#         env_vars={"PROJECT": PROJECT, "STAGING_BUCKET": STAGING_BUCKET, "AWS_CREDS": "/etc/aws/aws-creds.json"
-#                  , "GOOGLE_APPLICATION_CREDENTIALS": "/etc/gcp/keyfile.json"},
-#     )
+    from_s3_to_gcs = GKEStartPodOperator(
+        # The ID specified for the task.
+        task_id="data-transfer-task",
+        # Name of task you want to run, used to generate Pod ID.
+        name="data-transfer-task",
+        project_id=PROJECT,
+        location=CLUSTER_REGION,  # type: ignore
+        cluster_name=CLUSTER_NAME,
+        cmds=["/bin/bash", "./extract_data.sh", "yellow"],
+        namespace="default",
+        image="eu.gcr.io/stella-luxury-taxi/transfer-pod1",
+        secrets=[gcp_secret],
+        env_vars={"PROJECT": PROJECT, "STAGING_BUCKET": STAGING_BUCKET, "AWS_CREDS": "/etc/aws/aws-creds.json"
+                 , "GOOGLE_APPLICATION_CREDENTIALS": "/etc/gcp/keyfile.json"},
+    )
 
-#     from_s3_to_gcs
+    from_s3_to_gcs
