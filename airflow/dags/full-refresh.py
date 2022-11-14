12fe332f6a08e7af8221a1e542b8dd298c28a325
Islam Elsayed <107177143+Liftingthedata@users.noreply.github.com>
2022-11-14 17:29:14 +0200

    Update full-refresh.py

--- airflow/dags/full-refresh.py
+++ airflow/dags/full-refresh.py
@@ -66,7 +66,7 @@ with DAG(
         cmds=["/bin/bash", "./extract_data.sh", "yellow"],
         namespace="default",
         image="eu.gcr.io/stella-luxury-taxi/transfer-pod",
-        servuce_account_name="gkesa"
+        servuce_account_name="gkesa",
         secrets=[aws_secret],
         env_vars={"PROJECT": PROJECT, "STAGING_BUCKET": STAGING_BUCKET, "AWS_CREDS": "/etc/aws/aws-creds.json"
                  , "GOOGLE_APPLICATION_CREDENTIALS": "/etc/gcp/keyfile.json"},
